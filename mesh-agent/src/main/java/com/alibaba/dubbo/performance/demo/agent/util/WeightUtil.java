b7ec37ca145923553b5310b24bf09a1c5b0dd673
shangzonghai <shangzonghai@youzan.com>
2018-06-06 01:32:29 +0800

    修改provider调用比例1:2:3

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/util/WeightUtil.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/util/WeightUtil.java
@@ -7,26 +7,28 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author 景竹 2018/5/12
  */
 public class WeightUtil {
-    private static int[] ports = new int[]{30000, 30001, 30002, 30001, 30002, 30002};
+    private static int[] ports = new int[]{30000, 30001, 30001, 30001, 30002, 30002, 30002, 30002};
     //private static int[] ports = new int[]{30000};
     private static Random random = new Random();
     private static AtomicInteger atomicInteger = new AtomicInteger(0);
     private static int i = 0;
 
-    public static int getRandom(){
+    public static int getRandom() {
         return ports[random.nextInt(ports.length)];
     }
 
-    public static int getId(){
+    public static int getId() {
         return atomicInteger.getAndIncrement();
     }
+
     /**
      * i++有线程不安全，然而并没有什么大影响
      */
-    public static int getRandom2(){
+    public static int getRandom2() {
         return ports[i++ % ports.length];
     }
-    public static int getRandom(int id){
+
+    public static int getRandom(int id) {
         return ports[id % ports.length];
     }
 }
