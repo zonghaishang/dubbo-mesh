31eb3f6f35474983d0fe39b9b46e4ca05e6371f8
景竹 <jacky.ll@antfin.com>
2018-06-14 12:50:31 +0800

    去掉了group 写死loop数量为16

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/provider/ProviderService.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/provider/ProviderService.java
@@ -25,9 +25,9 @@ public class ProviderService {
         new EtcdRegistry(System.getProperty(Constants.ETCE)).register(Constants.SERVER_NAME,providerServerPort);
         ServerBootstrap bootstrap = new ServerBootstrap();
         EventLoopGroup boss = new NioEventLoopGroup(Constants.EVENT_LOOP_NUM);
-        EventLoopGroup boss0 = new NioEventLoopGroup(Constants.EVENT_LOOP_NUM);
+        //EventLoopGroup boss0 = new NioEventLoopGroup(Constants.EVENT_LOOP_NUM);
         //((NioEventLoopGroup) boss).setIoRatio(100);
-        bootstrap.group(boss0,boss)
+        bootstrap.group(boss)
                 .channel(NioServerSocketChannel.class)
                 .option(ChannelOption.ALLOCATOR,PooledByteBufAllocator.DEFAULT)
                 .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator( Constants.FIXED_RECV_BYTEBUF_ALLOCATOR))
